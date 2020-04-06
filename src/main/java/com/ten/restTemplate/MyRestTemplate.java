1bde75e39dba5058304ca68f86b8c9df4d52e796
evte0414 <friday13_-08@mail.ru>
2020-04-07 00:00:53 +0400

    crud operations working

--- src/main/java/com/ten/restTemplate/MyRestTemplate.java
+++ src/main/java/com/ten/restTemplate/MyRestTemplate.java
@@ -2,18 +2,32 @@ package com.ten.restTemplate;
 
 import com.ten.model.Role;
 import com.ten.model.User;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
 @Transactional
 public class MyRestTemplate {
 
+    private final RestTemplate restTemplate;
+
+    public MyRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder
+                .basicAuthentication("admin", "admin")
+                .build();
+    }
+
     public List<User> selectAllUsers() {
         final String uri = "http://localhost:8081/server/getAllUsers";
-        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
         List<User> result = restTemplate.getForObject(uri, List.class);
         return  result;
         // верно я поняла что в URI должен быть адрес сервера? занчит на сервер приложении нужен еще один рест контроллер
@@ -22,20 +36,20 @@ public class MyRestTemplate {
     }
 
      public void removeUser(Long id) {
-         RestTemplate restTemplate = new RestTemplate();
+         //RestTemplate restTemplate = new RestTemplate();
          String url = "http://localhost:8081/server/delete/{id}";
          restTemplate.delete(url, id);
      }
 
      public void addUser(User user) {
-         RestTemplate restTemplate = new RestTemplate();
+         //RestTemplate restTemplate = new RestTemplate();
          String uri = "http://localhost:8081/server/create";
          restTemplate.postForEntity(uri, user, User.class);
          //restTemplate.postForObject(uri, user,User.class);
      }
 
     public void updateUser(User user) {
-        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
         String uri = "http://localhost:8081/server/doUpdate";
         restTemplate.postForEntity(uri, user, User.class);
         //restTemplate.postForObject(uri, user,User.class);
@@ -44,13 +58,13 @@ public class MyRestTemplate {
 
 
      public User getUserById(long id) {
-        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
         String uri = "http://localhost:8081/server/getUserById/{id}";
         return restTemplate.getForObject(uri, User.class, id);
      }
 
     public Role getRoleById(Long id) {
-        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
         String uri = "http://localhost:8081/server/getRoleById/{id}";
         return restTemplate.getForObject(uri, Role.class, id);
     }
@@ -59,18 +73,9 @@ public class MyRestTemplate {
 //        return em.find(Role.class, id);
 //    }
 
-
-
-
-
-
-
     public User getUserByLogin(String login) {
-        //тут надо назанчить юри с адресом метода в рест окнтрллере на сервере
-        //тут нужно вызвать рест контроллер на сервре спец мтеод который примет стринг и будет вызвать метод в дао
-        final String uri = "http://localhost:8081/server/getAllUsers";
-        RestTemplate restTemplate = new RestTemplate();
-        User user = restTemplate.getForObject(uri, User.class);
-        return user;
+        //RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8081/server/getLoggedUser/{login}";
+        return restTemplate.getForObject(uri, User.class, login);
     }
 }
